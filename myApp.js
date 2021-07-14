4fcb3ba0ae902023ecef748eb537843c36fe2f91
kimSpiegel04 <k.spiegel72@gmail.com>
2021-07-14 13:29:00 -0400

    connect mongoose

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const mongodb = require('mongodb');
 
-
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
